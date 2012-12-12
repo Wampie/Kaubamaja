8cdacc7323016e466fa15570c93de5f3ad7f8818
Aleksi J Majander <ajmajand@e42-20.cs.helsinki.fi>
2012-12-12 14:54:38 +0200

    initial commit and stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaubamaja::Application.config.secret_token = '3f68d20726d7a9601fed7c1a6fbc0489a0db376f19f0fed16fd21e35a10f1d29dd2eb6890e1da75446e6f4f83be494dba2ba7cd4f379b77c6e347cdaf849d7b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kaubamaja::Application.config.session_store :cookie_store, key: '_kaubamaja_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kaubamaja::Application.config.session_store :active_record_store
